fe208cd2542f76d40b2d558659112d3ac32d92b6
TeeGreat <toyinatofarati64@gmail.com>
2025-02-14 22:10:40 +0100

    Re-added client folder

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+
+export default function Home() {
+  return <div className="nondashboard-layout">
+    <NonDashboardNavbar />
+  </div>;
+}
