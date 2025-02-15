9e13450888348dd9075d8f055c4f4661630cf26c
TeeGreat <toyinatofarati64@gmail.com>
2025-02-15 08:11:23 +0100

    landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,15 @@
 import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Landing from "./(nondashboard)/landing/page";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  return <div className="nondashboard-layout">
-    <NonDashboardNavbar />
-  </div>;
+  return (
+    <div className="nondashboard-layout">
+      <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+      <Footer />
+    </div>
+  );
 }
