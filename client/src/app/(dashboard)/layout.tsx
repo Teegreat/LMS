0701e67aa5a6ccbb3d935ecdace0b027868ff3d6
TeeGreat <toyinatofarati64@gmail.com>
2025-02-21 10:30:48 +0100

    app sidebar

---
+++ client/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Footer from "@/components/Footer";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import Loading from "@/components/Loading";
+import { cn } from "@/lib/utils";
+import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const [courseId, setCourseId] = useState<string | null>(null);
+  const { user, isLoaded } = useUser();
+
+  // handle useEffect isCoursePage
+
+  if (!isLoaded) return <Loading />;
+  if (!user) return <div>Please sign in to access this page.</div>;
+
+  return (
+    <SidebarProvider>
+      <div className="dashboard">
+        <AppSidebar />
+
+        <div className="dashboard__content">
+          {/* chapter | sidebar */}
+          <div className={cn("dashboard__main")} style={{ height: "100vh" }}>
+            <main className="dashboard__body">{children}</main>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
